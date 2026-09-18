7377dd66aa81fc49b1c8a79ede58e1b7a0c62844
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-09-18 09:36:03 -0700

    feat: Add macOS support for Flutter application
    - Added app icon for macOS application.
    - Created MainMenu.xib for the macOS app interface.
    - Configured AppInfo.xcconfig with application metadata.
    - Set up Debug and Release configuration files.
    - Defined warning settings in Warnings.xcconfig.
    - Implemented entitlements for Debug and Release builds.
    - Created Info.plist for application properties.
    - Developed MainFlutterWindow.swift to manage the main window.
    - Added unit tests in RunnerTests.swift for future code validation.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,34 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_drop
+import file_picker
+import file_selector_macos
+import media_kit_libs_macos_video
+import media_kit_video
+import package_info_plus
+import record_darwin
+import screen_retriever_macos
+import video_player_avfoundation
+import volume_controller
+import wakelock_plus
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
+  MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
+  VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
